1df074daf7eace5587a86b749c7467d703c634c3
Josh Langsfeld <jdlangs@gmail.com>
2015-05-20 23:00:31 -0400

    another fix

--- deps/build.jl
+++ deps/build.jl
@@ -85,7 +85,7 @@ else
         write(f,"""# This is an auto-generated file; do not edit\n""")
         write(f,"""# Macro to load a library\n""")
         write(f,"""macro checked_lib(libname, path)\n""")
-        write(f,"""    (dlopen_e(path) == C_NULL) && error("Unable to load \\n\\n\$libname (\$path)\\n\\nPlease re-run Pkg.build(package), and restart Julia.")\n""")
+        write(f,"""    (Libdl.dlopen_e(path) == C_NULL) && error("Unable to load \\n\\n\$libname (\$path)\\n\\nPlease re-run Pkg.build(package), and restart Julia.")\n""")
         write(f,"""    quote const \$(esc(libname)) = \$path end\n""")
         write(f,"""end\n""")
         write(f,"""\n""")
